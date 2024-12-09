20deb35cd5acf394f113a47c8df639aec3cfa6ed
Rodrigo Alarcón <rodricsxd@gmail.com>
2024-12-09 00:03:49 -0300

    adding shorten url functionality

--- app/[short_code]/page.tsx
+++ app/[short_code]/page.tsx
@@ -1,24 +1,28 @@
 import { supabase } from "@/lib/supabaseClient";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function ShortCodePage({
-    params,
-  }: {
-    params: { short_code: string };
-  }) {
-    
+  params,
+}: {
+  params: { short_code: string };
+}) {
   const short_code = params.short_code;
 
-  console.log(short_code)
+  useEffect(() => {
+    async function fetchUrl() {
+      const { data, error } = await supabase
+        .from("urls")
+        .select("original_url")
+        .eq("short_code", short_code);
 
-  const { data, error } = await supabase
-    .from("urls")
-    .select("original_url")
-    .eq("short_code", short_code);
+      if (error || !data || data.length == 0) {
+        redirect("/");
+      }
 
-  if (error || !data || data.length == 0) {
-    redirect("/")
-  }
+      redirect(data[0].original_url);
+    }
+  }, [short_code]);
 
-  redirect(data[0].original_url);
+  return <div>Cargando...</div>;
 }
