b5d16dac256bba775a909a0d09639fe3795c1bd0
Rodrigo Alarcón <rodricsxd@gmail.com>
2024-12-08 23:59:41 -0300

    adding shorten url functionality

--- app/[short_code]/page.tsx
+++ app/[short_code]/page.tsx
@@ -9,13 +9,15 @@ export default async function ShortCodePage({
     
   const short_code = params.short_code;
 
+  console.log(short_code)
+
   const { data, error } = await supabase
     .from("urls")
     .select("original_url")
     .eq("short_code", short_code);
 
-  if (error || !data) {
-    redirect("/404");
+  if (error || !data || data.length == 0) {
+    redirect("/")
   }
 
   redirect(data[0].original_url);
